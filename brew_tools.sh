c655d481c82c2779f6fd83f79fbf777d4b140a6a
suren <suren@git>
2023-04-08 12:03:39 +0100

    vs code mac

--- brew_tools.sh
+++ brew_tools.sh
@@ -5,11 +5,14 @@ tools=(
        "cocoapods"
        "exa"
        "gh"
+       "gopls"
+       "dlv"
        "helix" 
        "helm"
        "kustomize"
        "kubernetes-cli"
        "lf"
+       "lazygit"
        "minikube"
        "nvm"
        "ruby"
@@ -21,3 +24,8 @@ for i in ${tools[@]}; do
   echo installing $i
   brew install $i
 done
+
+echo "fonts"
+
+brew tap homebrew/cask-fonts
+brew install font-hack-nerd-font
